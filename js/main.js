35463cf1b62c4ba6533931b51317d27d8b09d016
Alice Yuan <alice.yuan@xtremelabs.com>
2013-07-13 16:47:47 -0400

    obstacles added

--- js/main.js
+++ js/main.js
@@ -25,6 +25,13 @@ $(function() {
 		r: 10
 	};
 
+
+	world['obstacle'] = {
+		x: 600,
+		y: 390,
+		r: 20
+	};
+
 	var initialWorld = $.extend(true, {}, world); // deep copy
 
 	var isAnimating = false;
@@ -55,18 +62,44 @@ $(function() {
 		w_context.fillRect(0, world.height - 10, world.width, world.height);
 	};
 
+	var drawObstacle = function(size, obstacle){
+		var obsColor = 'grey';
+		var obsStrokeW = 2;
+		var obsStrokeColor = 'black';
+
+		w_context.beginPath();
+		w_context.arc(obstacle.x, obstacle.y, obstacle.r, Math.PI, Math.PI*2, false);
+		w_context.closePath();
+		w_context.fillStyle = obsColor;
+		w_context.fill();
+		w_context.lineWidth = obsStrokeW;
+		w_context.strokeStyle = obsStrokeColor;
+		w_context.stroke();
+
+	};
+
+	var isCloseToObstacle = function(player, obstacle) {
+		if (player.x > (obstacle.x - obstacle.r*2) && player.x < (obstacle.x + obstacle.r*2)) {
+			return true;
+		}
+		return false;
+	};
+
 	var updateWorld = function() {
 		if (isAnimating) {
-			world.player.x += world.player.speed.x;
-			world.player.y += world.player.speed.y;
+			if (!isCloseToObstacle(world.player, world.obstacle)){
+				world.player.x += world.player.speed.x;
+				world.player.y += world.player.speed.y;
+			}
 		}
 	};
 
 	var draw = function() {
 		updateWorld();
-
 		drawEarth();
-		drawPlayer(world.player);
+		drawObstacle(1, world.obstacle);
+		if (world.player.x < world.width)
+			drawPlayer(world.player);
 
 		// TODO(cbhl): cross-browser compatibility
 		window.requestAnimationFrame(draw);
@@ -101,6 +134,7 @@ $(function() {
         $("<div>").addClass('btn btn-large cmd-left icon-arrow-left'),
         $("<div>").addClass('btn btn-large cmd-up icon-arrow-up')
     ];
+
 
     $('#command').append(buttons);
 });
